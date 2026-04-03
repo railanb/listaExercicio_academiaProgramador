633cba1dac6386295fa7a53a4ea2ca3f109366f1
railanb <railanb.rb@hotmail.com>
2026-04-02 21:29:16 -0300

    Exercicio 03

---
+++ Exercicio03.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+Console.Clear();
+
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Vamos converter as escalas Celsius para Fahrenheit");
+Console.WriteLine("--------------------------------------------------");
+
+
+Console.Write("\n=> Quantos graus °C esta agora: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+double fahrenheit = (celsius * 1.8) + 32;
+
+Console.WriteLine($"\n=> Na escala Fahrenheit, agora está: {fahrenheit:F2} °F");
+Console.ReadLine();
