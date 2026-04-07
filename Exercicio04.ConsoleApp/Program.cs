256db9fa4c1e670690d58a3f9a1fb1912e860cd1
railan <railanb.rb@hotmail.com>
2026-04-07 09:20:35 -0300

    exercicio 04

--- Exercicio04.ConsoleApp/Program.cs
+++ Exercicio04.ConsoleApp/Program.cs
@@ -1,1 +1,48 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+    Crie um programa para calcular o salário total de um vendedor. 
+    Deverá ser informado o salário base e o total de vendas. 
+    A comissão é calculada com um percentual (informado pelo usuário) sobre o total de vendas.
+
+*/
+bool repetirCalculo = true;
+
+while (repetirCalculo == true)
+{
+    Console.Clear();
+    Console.WriteLine("###################################");
+    Console.WriteLine("##    CALCULADORA DE COMISSÃO    ##");
+    Console.WriteLine("###################################");
+    
+    Console.Write(" => Qual é o salario base: ");
+    double salariobase = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(" => Qual foi o valor total de vendas: ");
+    double totalVendas = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write(" => Qual é o percentual de comissão: ");
+    double porcentagemComissao = Convert.ToDouble(Console.ReadLine());
+
+    double porcentagem = porcentagemComissao / 100;
+
+    double salarioComissao = totalVendas * porcentagem;
+    double salarioTotal = salarioComissao + salariobase;
+
+    Console.WriteLine($"-- Seu vendedor teve uma comissão de R$ {salarioComissao:F2}");
+    Console.WriteLine($"-- Tendo um salario total de R$ {salarioTotal:F2}");
+
+    Console.ReadLine();
+    
+    Console.Write("\nDeseja fazer um novo calculo? [s/N] => ");
+    string decisao = Console.ReadLine();
+
+    if (decisao != "s" && decisao != "S")
+    {
+        repetirCalculo = false;
+        Console.WriteLine("Saindo da calculadora...");
+        break;
+    }
+
+    
+}
+
+Console.ReadLine();
