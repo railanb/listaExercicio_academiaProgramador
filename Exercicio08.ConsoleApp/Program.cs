64fb77f1e1af91a8ccab76f04f3b303d83073172
railan <railanb.rb@hotmail.com>
2026-04-07 13:50:08 -0300

    exercicio 08

--- Exercicio08.ConsoleApp/Program.cs
+++ Exercicio08.ConsoleApp/Program.cs
@@ -3,3 +3,18 @@ Console.Clear();
 Console.WriteLine("###################################");
 Console.WriteLine("#         É IMPAR OU PAR?         #");
 Console.WriteLine("###################################");
+
+Console.Write("Digite algum número inteiro: ");
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+int numero = numeroDigitado % 2;
+
+if (numero == 0)
+{
+    Console.WriteLine($"O Número digitado é PAR");
+} else
+{
+    Console.WriteLine($"O Número digitado é IMPAR");
+}
+
+Console.ReadLine();
