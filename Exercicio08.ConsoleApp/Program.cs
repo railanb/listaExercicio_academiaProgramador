26f9c64782099c5f4544717167e9bc36dcfc7a99
railan <railanb.rb@hotmail.com>
2026-04-07 12:00:26 -0300

    iniciando exercicio 08

---
+++ Exercicio08.ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿/*Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.*/
+Console.Clear();
+Console.WriteLine("###################################");
+Console.WriteLine("#         É IMPAR OU PAR?         #");
+Console.WriteLine("###################################");
